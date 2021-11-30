4480db34781f23d313cb927c81011ac8b25947f1
jkjellan <jkjellan@gmail.com>
2021-11-29 21:42:37 -0800

    begin to implement shaders

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import * as THREE from "three";
-import earth from "./earth.jpeg"
+import earth from "./earth.jpeg";
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
 
 class App extends Component {
   constructor(props) {
@@ -11,24 +13,26 @@ class App extends Component {
       // === THREE.JS CODE START ===
       var scene = new THREE.Scene();
       var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-      var renderer = new THREE.WebGLRenderer();
+      var renderer = new THREE.WebGLRenderer({antialias: true});
       renderer.setSize( window.innerWidth, window.innerHeight );
+      renderer.setPixelRatio(window.devicePixelRatio);
       this.threeJsMountDiv.appendChild( renderer.domElement );
 
       var sphere = new THREE.Mesh( 
         new THREE.SphereGeometry( 5, 50, 50 ),
-        new THREE.MeshBasicMaterial( { 
-          map: new THREE.TextureLoader().load(earth)
+        new THREE.ShaderMaterial({
+          vertexShader,
+          fragmentShader
         }) 
       );
 
       console.log(sphere)
       scene.add( sphere );
-      camera.position.z = 20;
+      camera.position.z = 10;
       var animate = function () {
         requestAnimationFrame( animate );
-        sphere.rotation.x += 0.01;
-        sphere.rotation.y += 0.01;
+        //sphere.rotation.x += 0.01;
+        sphere.rotation.y += 0.001;
         renderer.render( scene, camera );
       };
       animate();
